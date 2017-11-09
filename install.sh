c23d78fcc1332ef858741fcf634102368fec0645
leewi9 <leewi9@leewi9s-MacBook-Pro.local>
2017-11-09 14:44:20 +0800

    first commit

---
+++ install.sh
@@ -0,0 +1,17 @@
+sudo mkdir -p /data/backups/mysql
+cd /data/backups/mysql
+
+sudo wget https://repo.percona.com/apt/percona-release_0.1-4.$(lsb_release -sc)_all.deb
+sudo dpkg -i percona-release_0.1-4.$(lsb_release -sc)_all.deb
+sudo apt-get update
+sudo apt-get -y install percona-xtrabackup-24
+
+sudo apt-get -y install qpress
+
+# 制作密钥
+printf '%s' "$(openssl rand -base64 24)" | sudo tee /data/backups/mysql/encryption_key && echo
+
+#
+sudo chmod +x backup-mysql.sh
+sudo chmod +x extract-mysql.sh
+sudo chmod +x prepare-mysql.sh
